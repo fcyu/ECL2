dbdd04b6a63a7eddb9f409d0b46788c0a5fc5735
Fengchao <fyuab@connect.ust.hk>
2017-01-22 15:31:31 +0800

    Improve batch and multithread assigment.

--- src/main/java/proteomics/Search/SearchWrap.java
+++ src/main/java/proteomics/Search/SearchWrap.java
@@ -4,9 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import proteomics.ECL2;
 import proteomics.Index.BuildIndex;
+import proteomics.Spectrum.PreSpectrum;
 import proteomics.TheoSeq.MassTool;
 import proteomics.Types.FinalResultEntry;
 import proteomics.Types.ResultEntry;
+import proteomics.Types.SparseVector;
 import proteomics.Types.SpectrumEntry;
 
 import java.util.*;
@@ -17,53 +19,47 @@ public class SearchWrap implements Callable<List<FinalResultEntry>> {
     private static final Logger logger = LoggerFactory.getLogger(SearchWrap.class);
 
     private final Search search_obj;
-    private final NavigableMap<Integer, Set<SpectrumEntry>> bin_spectra_map;
-    private final Map<Integer, SpectrumEntry> num_spectrum_map;
+    private final Map<Integer, SpectrumEntry> numSpectrumMap;
+    private final Integer[] scanNumArray;
     private final BuildIndex build_index_obj;
     private final MassTool mass_tool_obj;
     private final int max_common_ion_charge;
+    private final PreSpectrum preSpectrumObj;
 
-    public SearchWrap(Search search_obj, NavigableMap<Integer, Set<SpectrumEntry>> bin_spectra_map, Map<Integer, SpectrumEntry> num_spectrum_map, BuildIndex build_index_obj, MassTool mass_tool_obj, int max_common_ion_charge) {
+    public SearchWrap(Search search_obj, Map<Integer, SpectrumEntry> numSpectrumMap, Integer[] scanNumArray, BuildIndex build_index_obj, MassTool mass_tool_obj, int max_common_ion_charge) {
         this.search_obj = search_obj;
-        this.bin_spectra_map = bin_spectra_map;
-        this.num_spectrum_map = num_spectrum_map;
+        this.numSpectrumMap = numSpectrumMap;
+        this.scanNumArray = scanNumArray;
         this.build_index_obj = build_index_obj;
         this.mass_tool_obj = mass_tool_obj;
         this.max_common_ion_charge = max_common_ion_charge;
+        preSpectrumObj = new PreSpectrum(mass_tool_obj);
     }
 
     @Override
     public List<FinalResultEntry> call() {
-        try {
-            if (!bin_spectra_map.isEmpty()) {
-                List<FinalResultEntry> outputList = new LinkedList<>();
-                Map<Integer, ResultEntry> search_result =  search_obj.doSearch(bin_spectra_map);
-                for (int scan_num : search_result.keySet()) {
-                    ResultEntry result_entry = search_result.get(scan_num);
-                    if (1 - (result_entry.getSecondScore() / result_entry.getScore()) > ECL2.delta_c_t) {
-                        if (ECL2.cal_evalue) {
-                            float e_value_precursor_mass_tol;
-                            if (search_obj.ms1_tolerance_unit == 1) {
-                                e_value_precursor_mass_tol = result_entry.spectrum_mass * search_obj.ms1_tolerance * 1e-6f;
-                            } else {
-                                e_value_precursor_mass_tol = search_obj.ms1_tolerance;
-                            }
-                            new CalEValue(scan_num, result_entry, num_spectrum_map.get(scan_num).pl_map_xcorr, build_index_obj.getUniprotDecoyMassSeqMap(), mass_tool_obj, build_index_obj.linker_mass, max_common_ion_charge, search_obj.consider_two_identical_chains, e_value_precursor_mass_tol);
-                            if (result_entry.getEValue() != 9999) {
-                                outputList.add(search_obj.convertResultEntry(scan_num, result_entry));
-                            }
+        List<FinalResultEntry> outputList = new LinkedList<>();
+        for (int scanNum : scanNumArray) {
+            SpectrumEntry spectrumEntry = numSpectrumMap.get(scanNum);
+            SparseVector xcorrPL = preSpectrumObj.prepareXcorr(spectrumEntry.originalPlMap, spectrumEntry.precursor_mass);
+            ResultEntry resultEntry =  search_obj.doSearch(spectrumEntry, xcorrPL);
+            if (resultEntry != null) {
+                if (1 - (resultEntry.getSecondScore() / resultEntry.getScore()) > ECL2.delta_c_t) {
+                    if (ECL2.cal_evalue) {
+                        float e_value_precursor_mass_tol;
+                        if (search_obj.ms1_tolerance_unit == 1) {
+                            e_value_precursor_mass_tol = resultEntry.spectrum_mass * search_obj.ms1_tolerance * 1e-6f;
+                        } else {
+                            e_value_precursor_mass_tol = search_obj.ms1_tolerance;
+                        }
+                        new CalEValue(scanNum, resultEntry, xcorrPL, build_index_obj.getUniprotDecoyMassSeqMap(), mass_tool_obj, build_index_obj.linker_mass, max_common_ion_charge, search_obj.consider_two_identical_chains, e_value_precursor_mass_tol);
+                        if (resultEntry.getEValue() != 9999) {
+                            outputList.add(search_obj.convertResultEntry(scanNum, resultEntry));
                         }
                     }
                 }
-
-                System.gc();
-                return outputList;
             }
-        } catch (Exception ex) {
-            logger.error(ex.getMessage());
-            ex.printStackTrace();
-            System.exit(1);
         }
-        return null;
+        return outputList;
     }
 }
--- src/main/java/proteomics/Spectrum/PreSpectra.java
+++ src/main/java/proteomics/Spectrum/PreSpectra.java
@@ -2,11 +2,10 @@ package proteomics.Spectrum;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import proteomics.ECL2;
 import proteomics.Index.BuildIndex;
 import proteomics.TheoSeq.MassTool;
-import proteomics.Types.SparseVector;
 import proteomics.Types.SpectrumEntry;
-import uk.ac.ebi.pride.tools.jmzreader.*;
 import uk.ac.ebi.pride.tools.jmzreader.model.*;
 import uk.ac.ebi.pride.tools.mzxml_parser.MzXMLFile;
 import uk.ac.ebi.pride.tools.mzxml_parser.MzXMLParsingException;
@@ -16,17 +15,34 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PreSpectra {
 
     private static final Logger logger = LoggerFactory.getLogger(PreSpectra.class);
+    private static final Pattern pattern = Pattern.compile("^[0-9]+$");
 
-    private TreeMap<Integer, Set<SpectrumEntry>> bin_spectra_map = new TreeMap<>();
     private Map<Integer, SpectrumEntry> num_spectrum_map = new HashMap<>();
+    private Set<Integer> debug_scan_num_set = new HashSet<>();
 
-    public PreSpectra(MzXMLFile spectra_parser, BuildIndex build_index_obj, Map<String, String> parameter_map, MassTool mass_tool_obj, FilterSpectra.MassScan[] scan_num_array, int idx_start, int idx_end) {
+    public PreSpectra(MzXMLFile spectra_parser, BuildIndex build_index_obj, Map<String, String> parameter_map, MassTool mass_tool_obj) {
+        int min_ms1_charge = Integer.valueOf(parameter_map.get("min_ms1_charge"));
+        int max_ms1_charge = Integer.valueOf(parameter_map.get("max_ms1_charge"));
+        float min_precursor_mass =  Float.valueOf(parameter_map.get("min_precursor_mass"));
+        float max_precursor_mass = Float.valueOf(parameter_map.get("max_precursor_mass"));
         int min_peak_num = Integer.valueOf(parameter_map.get("min_peak_num"));
         Map<String, Float> mass_table = mass_tool_obj.getMassTable();
+
+        //  In DEBUG mode, filter out unlisted scan num
+        if (ECL2.debug) {
+            for (String k : parameter_map.keySet()) {
+                Matcher matcher = pattern.matcher(k);
+                if (matcher.find()) {
+                    debug_scan_num_set.add(Integer.valueOf(k));
+                }
+            }
+        }
 
         PreSpectrum pre_spectrum_obj = new PreSpectrum(mass_tool_obj);
 
@@ -37,51 +53,63 @@ public class PreSpectra {
         });
         System.setOut(null_stream);
 
-        int idx = idx_start;
+        Iterator<Spectrum> spectrumIterator = spectra_parser.getSpectrumIterator();
         try {
-            while ((idx < idx_end) && (idx < scan_num_array.length)) {
-                Spectrum spectrum = spectra_parser.getSpectrumById(scan_num_array[idx].scan_id);
-                int precursor_charge = spectrum.getPrecursorCharge();
-                float precursor_mz = spectrum.getPrecursorMZ().floatValue();
-                float precursor_mass = precursor_mz * precursor_charge - precursor_charge * mass_table.get("PROTON");
-                Map<Double, Double> raw_mz_intensity_map = spectrum.getPeakList();
-                SparseVector pl_map = pre_spectrum_obj.preSpectrum(raw_mz_intensity_map, precursor_mass);
-                if (pl_map.getNonzeroNum() <= min_peak_num) {
-                    ++idx;
+            while (spectrumIterator.hasNext()) {
+                Spectrum spectrum = spectrumIterator.next();
+
+                if (ECL2.debug && !debug_scan_num_set.contains(Integer.valueOf(spectrum.getId()))) {
                     continue;
                 }
 
-                int scan_num = Integer.valueOf(scan_num_array[idx].scan_id);
+                if (spectrum.getMsLevel() != 2) {
+                    continue;
+                }
+
+                if (spectrum.getPrecursorCharge() == null) {
+                    logger.warn("Scan {} doesn't have charge information. Skip.", spectrum.getId());
+                    continue;
+                }
+                int precursor_charge = spectrum.getPrecursorCharge();
+
+                if ((precursor_charge < min_ms1_charge) || (precursor_charge > max_ms1_charge)) {
+                    continue;
+                }
+
+                float precursor_mz = spectrum.getPrecursorMZ().floatValue();
+                float precursor_mass = precursor_mz * precursor_charge - precursor_charge * mass_table.get("PROTON");
+
+                if ((precursor_mass > max_precursor_mass) || (precursor_mass < min_precursor_mass)) {
+                    continue;
+                }
+
+                Map<Double, Double> raw_mz_intensity_map = spectrum.getPeakList();
+
+                if (raw_mz_intensity_map.size() < min_peak_num) {
+                    logger.debug("Scan {} doesn't contain enough peak number ({}). Skip.", spectrum.getId(), min_peak_num);
+                    continue;
+                }
+
+                TreeMap<Float, Float> originalPlMap = pre_spectrum_obj.preSpectrum(raw_mz_intensity_map, precursor_mass);
+                if (originalPlMap.size() <= min_peak_num) {
+                    continue;
+                }
+
+                int scan_num = Integer.valueOf(spectrum.getId());
 
                 Scan scan = spectra_parser.getScanByNum((long) scan_num);
                 float rt = scan.getRetentionTime().getSeconds();
 
-                SpectrumEntry spectrum_entry = new SpectrumEntry(scan_num, spectrum.getId(), precursor_mz, precursor_mass, precursor_charge, rt, pl_map, build_index_obj.linker_mass);
+                SpectrumEntry spectrum_entry = new SpectrumEntry(scan_num, spectrum.getId(), precursor_mz, precursor_mass, precursor_charge, rt, originalPlMap, build_index_obj.linker_mass);
                 num_spectrum_map.put(scan_num, spectrum_entry);
-                int bin_idx = build_index_obj.massToBin(precursor_mass);
-
-                if (bin_spectra_map.containsKey(bin_idx)) {
-                    Set<SpectrumEntry> spectrum_set = bin_spectra_map.get(bin_idx);
-                    spectrum_set.add(spectrum_entry);
-                } else {
-                    Set<SpectrumEntry> scan_num_set = new HashSet<>();
-                    scan_num_set.add(spectrum_entry);
-                    bin_spectra_map.put(bin_idx, scan_num_set);
-                }
-
-                ++idx;
             }
-        } catch (JMzReaderException |MzXMLParsingException ex) {
+        } catch (MzXMLParsingException ex) {
             logger.error(ex.getMessage());
             ex.printStackTrace();
             System.exit(1);
         }
 
         System.setOut(original_stream);
-    }
-
-    public TreeMap<Integer, Set<SpectrumEntry>> returnMassNumMap() {
-        return bin_spectra_map;
     }
 
     public Map<Integer, SpectrumEntry> getNumSpectrumMap() {
