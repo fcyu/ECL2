5b9a1e610d59e59d642e0e39cb9e9f628aa4b25c
Fengchao <fyuab@connect.ust.hk>
2017-05-26 14:24:32 +0800

    Nightly build version.

--- src/main/java/proteomics/ECL2.java
+++ src/main/java/proteomics/ECL2.java
@@ -25,7 +25,7 @@ public class ECL2 {
     public static final boolean flankingPeaks = true;
 
     private static final Logger logger = LoggerFactory.getLogger(ECL2.class);
-    private static final String version = "2.1.3";
+    public static final String version = "2.1.4-dev-1c9b46a";
 
     public static boolean debug;
     public static boolean dev;
