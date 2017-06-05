1096e83f5a188fdc8bc1c4d9a27c179be4c51a66
Fengchao <fyuab@connect.ust.hk>
2017-06-05 13:58:38 +0800

    Minor improvement.

--- src/main/java/proteomics/Types/AA.java
+++ src/main/java/proteomics/Types/AA.java
@@ -4,18 +4,26 @@ public class AA {
 
     public final char aa;
     public final float delta_mass;
+    private final String toString;
+    private final int hashCode;
 
     public AA(char aa, float delta_mass) {
         this.aa = aa;
         this.delta_mass = delta_mass;
+        if (Math.abs(delta_mass) > 1e-6) {
+            toString = String.format("%c[%.2f]", aa, delta_mass);
+        } else {
+            toString = String.valueOf(aa);
+        }
+        hashCode = toString.hashCode();
     }
 
     public String toString() {
-        if (Math.abs(delta_mass) > 1e-6) {
-            return String.format("%c[%.2f]", aa, delta_mass);
-        } else {
-            return String.valueOf(aa);
-        }
+        return toString;
+    }
+
+    public int hashCode() {
+        return hashCode;
     }
 
     public boolean equals(Object other) {
